695bd7e2d803cb023c8c455bceb2bc7c64195a9c
Anna <anrosa.j@gmail.com>
2020-05-07 20:35:33 +0200

    init

---
+++ src/main/java/pl/parabar/deliveryregister01/DeliveryRegisterApplication.java
@@ -0,0 +1,13 @@
+package pl.parabar.deliveryregister01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/parabar/deliveryregister01/DeliveryRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.parabar.deliveryregister01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
